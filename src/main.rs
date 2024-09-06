b86d9745508cbe29e3bc82918cb02a15da4e96fa
Pierrick Curt <pierrickcurt@gmail.com>
2024-09-06 12:22:09 +0200

    feat: refactor speed computation to use interrupt instead of ADC raw measure

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 #![no_std]
 #![no_main]
 mod cli;
+use core::cell::RefCell;
+
 use cli::set_speed;
+use critical_section::Mutex;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Instant, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    analog::adc::{Adc, AdcConfig, Attenuation},
     clock::ClockControl,
-    gpio::{GpioPin, Io},
-    peripherals::{Peripherals, ADC1},
+    gpio::{GpioPin, Input, Io, Pull},
+    peripherals::Peripherals,
     prelude::*,
     system::SystemControl,
     timer::{timg::TimerGroup, ErasedTimer, OneShotTimer},
     uart::Uart,
 };
+
+static COUNTER: Mutex<RefCell<u64>> = Mutex::new(RefCell::new(0));
 
 // When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
 macro_rules! mk_static {
@@ -26,65 +30,35 @@ macro_rules! mk_static {
     }};
 }
 
-const THRESHOLD_FOR_WHITE_DIAL_MV_HIGH: u16 = 3000;
-const THRESHOLD_FOR_WHITE_DIAL_MV_LOW: u16 = 500;
+struct MovingAverage {
+    samples: [u32; 5],
+    index: usize,
+    count: usize,
+    sum: u32,
+}
 
-#[embassy_executor::task]
-async fn compute_speed(analog_pin: GpioPin<3>, adc: ADC1) {
-    // Create ADC instances
-    type AdcCal = esp_hal::analog::adc::AdcCalBasic<esp_hal::peripherals::ADC1>;
-    let mut adc1_config = AdcConfig::new();
-    let mut adc1_pin =
-        adc1_config.enable_pin_with_cal::<_, AdcCal>(analog_pin, Attenuation::Attenuation11dB);
-    let mut adc1: Adc<'_, _> = Adc::new(adc, adc1_config);
-
-    // Local variabels to manage states and durations
-    let mut is_in_white_dial: bool = false;
-    let mut prev_dial_state: bool = false;
-    let mut last_time_change_s = Instant::now();
-    let mut stop_is_sent: bool = false;
-
-    // Initilaize first position
-    let raw_value: u16 = nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap();
-    if raw_value < THRESHOLD_FOR_WHITE_DIAL_MV_LOW {
-        is_in_white_dial = true;
-        prev_dial_state = true;
+impl MovingAverage {
+    fn new() -> Self {
+        MovingAverage {
+            samples: [0; 5],
+            index: 0,
+            count: 0,
+            sum: 0,
+        }
     }
 
-    loop {
-        let raw_value: u16 = nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap();
-
-        if raw_value > THRESHOLD_FOR_WHITE_DIAL_MV_HIGH {
-            is_in_white_dial = false;
-        }
-        if raw_value < THRESHOLD_FOR_WHITE_DIAL_MV_LOW {
-            is_in_white_dial = true;
+    fn add_sample(&mut self, sample: u32) -> u32 {
+        if self.count == 5 {
+            self.sum -= self.samples[self.index];
+        } else {
+            self.count += 1;
         }
 
-        if is_in_white_dial != prev_dial_state {
-            //Update sate and compute speed
-            stop_is_sent = false;
-            prev_dial_state = is_in_white_dial;
-            let current_time_s = Instant::now();
+        self.samples[self.index] = sample;
+        self.sum += sample;
 
-            // One dial represents 1,3cm
-            let delta_t_us = (current_time_s.as_micros() - last_time_change_s.as_micros()) as f32;
-            // Compute speed in cm/s on a float
-            let speed = 1.30 * 10_000_000.0 / delta_t_us;
-            // Compute speed in km/h on a u32
-            let speed_km_h = (((speed * 3600.0) as u32) / 100_000) as u32;
-
-            // Update last time change
-            last_time_change_s = current_time_s;
-            set_speed(speed_km_h * 10);
-        }
-
-        // If wheel is not mooving since more than x seconds send a 0kms speed
-        if (Instant::now().as_secs() - last_time_change_s.as_secs()) > 2 && !stop_is_sent {
-            stop_is_sent = true;
-            set_speed(0);
-        }
-        Timer::after(Duration::from_millis(1)).await;
+        self.index = (self.index + 1) % 5;
+        self.sum / self.count as u32
     }
 }
 
@@ -95,7 +69,6 @@ async fn main(spawner: Spawner) {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
     let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
     let timer0 = OneShotTimer::new(timg0.timer0.into());
     let timers = [timer0];
@@ -103,18 +76,58 @@ async fn main(spawner: Spawner) {
     esp_hal_embassy::init(&clocks, timers);
 
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let dial_io = io.pins.gpio4;
+    let dial: Input<'_, GpioPin<4>> = Input::new(dial_io, Pull::None);
+    esp_hal::interrupt::enable(
+        esp_hal::peripherals::Interrupt::GPIO,
+        esp_hal::interrupt::Priority::Priority1,
+    )
+    .unwrap();
 
-    // Start compute_speed task
-    spawner
-        .spawn(compute_speed(io.pins.gpio3, peripherals.ADC1))
-        .ok();
+    spawner.spawn(detect_dial(dial)).unwrap();
 
     // Start the CLI task
     let serial0 = Uart::new(peripherals.UART0, &clocks, io.pins.gpio21, io.pins.gpio20).unwrap();
     spawner.spawn(cli::cli_run(serial0)).ok();
 
+    let mut prev_cnt: u64 = 0;
+    let mut last_time_change_s = Instant::now();
+    let mut avg_speed = MovingAverage::new();
+
     loop {
-        // Nothing to do for the moment
-        Timer::after(Duration::from_millis(5_000)).await;
+        let cnt = critical_section::with(|cs| *COUNTER.borrow_ref(cs));
+        if cnt != prev_cnt {
+            // we are mooving
+            let distance: u64 = cnt - prev_cnt;
+            let current_time_s = Instant::now();
+            let delta_t_us = current_time_s.as_micros() - last_time_change_s.as_micros();
+
+            last_time_change_s = current_time_s;
+            prev_cnt = cnt;
+            let speed_raw = distance * 10_000_000 / delta_t_us;
+
+            // Compute the relative speed in 0.1 km/h, coefficient has been set using the home trainer
+            let speed_km_h: u32 = (speed_raw / 10) as u32;
+            // Above 70kmh it must be an error on the sensor drop it
+            let mean = avg_speed.add_sample(speed_km_h); // Average on last 5 samples
+            set_speed(mean);
+        }
+
+        // If wheel is not mooving since more than 2 seconds send a 0kms speed
+        if (Instant::now().as_secs() - last_time_change_s.as_secs()) > 2 {
+            set_speed(0);
+        }
+
+        Timer::after(Duration::from_millis(20)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn detect_dial(mut dial: Input<'static, GpioPin<4>>) {
+    loop {
+        dial.wait_for_any_edge().await;
+        critical_section::with(|cs| {
+            *COUNTER.borrow_ref_mut(cs) += 1;
+        });
     }
 }
